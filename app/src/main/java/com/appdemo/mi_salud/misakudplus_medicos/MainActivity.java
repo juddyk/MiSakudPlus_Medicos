66c95ad9083d8b3d92017ad33f5bf8114640320b
juddyk <reina.jk@gmail.com>
2017-05-08 06:02:53 -0500

    Se carga el nombre desde firebase

--- app/src/main/java/com/appdemo/mi_salud/misakudplus_medicos/MainActivity.java
+++ app/src/main/java/com/appdemo/mi_salud/misakudplus_medicos/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +20,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.appdemo.mi_salud.misakudplus_medicos.UI_otras.*;
@@ -31,9 +34,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StreamDownloadTask;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -42,17 +48,21 @@ public class MainActivity extends AppCompatActivity {
     Button btnAction_Agenda;
     EditText et_Slogan;
     ImageView iv_foto;
+    TextView tv_nombre;
     //FireBase
     private DatabaseReference mDB;
     private static final String TAG = "logueo";
     private static final String TAG_medicos = "Medicos";
     private static final String TAG_foto = "foto";
     private static final String TAG_slogan = "slogan";
+    private static final String TAG_nombre1 = "nombre1";
+    private static final String TAG_apellido1 = "apellido1";
     //FIREBASE STORAGE
     FirebaseStorage storage;
     private StorageReference storageRef;
     //
-    String usuarioName,rutaFoto;
+    String usuarioId,rutaFoto,nombre,apellido;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,14 +73,15 @@ public class MainActivity extends AppCompatActivity {
         btnAction_Agenda=(Button) findViewById(R.id.idBtnAgenda);
         et_Slogan=(EditText) findViewById(R.id.sloganDoctor);
         iv_foto=(ImageView) findViewById(R.id.fotoDoctor);
+        tv_nombre=(TextView) findViewById(R.id.nombreDoctor);
 
         //FIREBASE STORAGE
         storage=FirebaseStorage.getInstance();
         storageRef = storage.getReferenceFromUrl("gs://mi-salud-5965a.appspot.com");
 
         //Establecer el Eslogan
-        usuarioName=getIntent().getStringExtra("usuario");
-        mDB= FirebaseDatabase.getInstance().getReference().child(TAG_medicos).child(usuarioName);
+        usuarioId=getIntent().getStringExtra("usuario");
+        mDB= FirebaseDatabase.getInstance().getReference().child(TAG_medicos).child(usuarioId);
         mDB.child(TAG_slogan).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
@@ -82,6 +93,28 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Failed to load data.",Toast.LENGTH_SHORT).show();
             }
         });
+        mDB.child(TAG_nombre1).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                nombre= (String) snapshot.getValue();  //prints "Do you have data? You'll love Firebase."
+            }
+            @Override public void onCancelled(DatabaseError error) {
+                Log.w(TAG, "loadPost:onCancelled", error.toException());
+                Toast.makeText(MainActivity.this, "Failed to load data.",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mDB.child(TAG_apellido1).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                apellido= (String) snapshot.getValue();  //prints "Do you have data? You'll love Firebase."
+                tv_nombre.setText("Dr. "+nombre+" "+apellido);
+            }
+            @Override public void onCancelled(DatabaseError error) {
+                Log.w(TAG, "loadPost:onCancelled", error.toException());
+                Toast.makeText(MainActivity.this, "Failed to load data.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         //Establecer la ruta(Firebase) de la foto
         mDB.child(TAG_foto).addValueEventListener(new ValueEventListener() {
@@ -124,7 +157,6 @@ public class MainActivity extends AppCompatActivity {
         noticias fragment = new noticias();
         ft.add(R.id.idFragmentNoticias, fragment);
         ft.commit();
-
 
         //Se asocia la acción de ver la agenda cuando se da CLICK en el boton de la Agenda
         btnAction_Agenda.setOnClickListener(new View.OnClickListener() {
