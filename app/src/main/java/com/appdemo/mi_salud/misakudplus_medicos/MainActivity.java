4ef88dfc5a26bab113418b0a961915ed9c1d1af6
juddyk <reina.jk@gmail.com>
2017-05-08 05:45:22 -0500

    Se hace logueo de usuario y se carga Foto y Slogan de FireBase

--- app/src/main/java/com/appdemo/mi_salud/misakudplus_medicos/MainActivity.java
+++ app/src/main/java/com/appdemo/mi_salud/misakudplus_medicos/MainActivity.java
@@ -1,30 +1,122 @@
 package com.appdemo.mi_salud.misakudplus_medicos;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.appdemo.mi_salud.misakudplus_medicos.UI_otras.*;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    //Objetos de la Interfaz
     Button btnAction_Agenda;
+    EditText et_Slogan;
+    ImageView iv_foto;
+    //FireBase
+    private DatabaseReference mDB;
+    private static final String TAG = "logueo";
+    private static final String TAG_medicos = "Medicos";
+    private static final String TAG_foto = "foto";
+    private static final String TAG_slogan = "slogan";
+    //FIREBASE STORAGE
+    FirebaseStorage storage;
+    private StorageReference storageRef;
+    //
+    String usuarioName,rutaFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Se instancias objetos del a interfaz
         btnAction_Agenda=(Button) findViewById(R.id.idBtnAgenda);
+        et_Slogan=(EditText) findViewById(R.id.sloganDoctor);
+        iv_foto=(ImageView) findViewById(R.id.fotoDoctor);
+
+        //FIREBASE STORAGE
+        storage=FirebaseStorage.getInstance();
+        storageRef = storage.getReferenceFromUrl("gs://mi-salud-5965a.appspot.com");
+
+        //Establecer el Eslogan
+        usuarioName=getIntent().getStringExtra("usuario");
+        mDB= FirebaseDatabase.getInstance().getReference().child(TAG_medicos).child(usuarioName);
+        mDB.child(TAG_slogan).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                String slgn= (String) snapshot.getValue();  //prints "Do you have data? You'll love Firebase."
+                et_Slogan.setText(slgn);
+            }
+            @Override public void onCancelled(DatabaseError error) {
+                Log.w(TAG, "loadPost:onCancelled", error.toException());
+                Toast.makeText(MainActivity.this, "Failed to load data.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Establecer la ruta(Firebase) de la foto
+        mDB.child(TAG_foto).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                rutaFoto= (String) snapshot.getValue();  //prints "Do you have data? You'll love Firebase."
+                if(rutaFoto!=null){
+                    final long ONE_MEGABYTE = 1024 * 1024;
+                    storageRef.child(rutaFoto).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Toast.makeText(getApplicationContext(),getResources().getString(R.string.load_foto),Toast.LENGTH_SHORT).show();
+                            Glide.with(getApplicationContext())
+                                    .load(uri)
+                                    .into(iv_foto);
+
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            Toast.makeText(getApplicationContext(),getResources().getString(R.string.load_no_foto),Toast.LENGTH_SHORT).show();
+                            // Handle any errors
+                        }
+                    });
+
+                }
+
+
+            }
+            @Override public void onCancelled(DatabaseError error) {
+                Log.w(TAG, "loadPost:onCancelled", error.toException());
+                Toast.makeText(MainActivity.this, "Failed to load data.",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //SE CARGA EL FRAGMENT PARA MOSTRAR LAS NOTICIAS
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -70,5 +162,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
 
 }
--- app/src/main/java/com/appdemo/mi_salud/misakudplus_medicos/UI_otras/logueo.java
+++ app/src/main/java/com/appdemo/mi_salud/misakudplus_medicos/UI_otras/logueo.java
@@ -1,21 +1,48 @@
 package com.appdemo.mi_salud.misakudplus_medicos.UI_otras;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.appdemo.mi_salud.misakudplus_medicos.MainActivity;
 import com.appdemo.mi_salud.misakudplus_medicos.R;
 import com.appdemo.mi_salud.misakudplus_medicos.UI_dialogos.DialogAvisoRegistro;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class logueo extends AppCompatActivity implements DialogAvisoRegistro.AvisoListener {
+
+    //Objetos de la interfaz
     Button ActionBtn_logueo;//Se crea boton de logueo
     Button ActionBtn_registro;//Se crea boton de registro
+    EditText et_Usuario;//Se crea objeto para leer el numero de documento
+    EditText et_Contrasena;//Se crea objeto para leer la contraseña
+
+    //Variable de SESION
+    private SharedPreferences.Editor preferenceEditor;
+    private static final int PREFERENCE_MODE_PRIVATE=0;
+    boolean flag=false;
+    //FireBase
+    private DatabaseReference mDB;
+    private static final String TAG = "logueo";
+    private static final String TAG_medicos = "Medicos";
+    private static final String TAG_contrasena = "psswrd";
+    //Variables Axuliares
+    public String uname,upass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,16 +50,71 @@ public class logueo extends AppCompatActivity implements DialogAvisoRegistro.Avi
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//Pantalla Completa
         setContentView(R.layout.activity_logueo);
 
+        //Preferencias (Variable de Sesion)
+        SharedPreferences preferenceSettings;
+        preferenceSettings = getPreferences(PREFERENCE_MODE_PRIVATE);
+        preferenceEditor=preferenceSettings.edit();
+
+        if(getIntent().getBooleanExtra("close",false)){
+            preferenceEditor.putBoolean("flag",false);
+            preferenceEditor.apply();
+        }
+        if(preferenceSettings.getBoolean("flag",flag)){
+            Intent i=new Intent(logueo.this,MainActivity.class);
+            i.putExtra("usuario",preferenceSettings.getString("user",""));
+            startActivity(i);
+            finish();
+        }
+
         //Instancias de la interfaz
         ActionBtn_logueo = (Button) findViewById(R.id.idBtnLogueo);//Se asocia con el boton de la interfaz
         ActionBtn_registro = (Button) findViewById(R.id.idBtnRegister);//Se asocia con el boton de la interfaz
+        et_Usuario = (EditText) findViewById(R.id.idUSER);//Se asocia con el editText de la interfaz
+        et_Contrasena = (EditText) findViewById(R.id.idPASSWORD);//Se asocia con el editText de la interfaz
 
         //Se asocia la acción de abrir la vista principal cuando se da CLICK en el boton de Iniciar Sesión
         ActionBtn_logueo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i =new Intent(logueo.this, MainActivity.class);
-                startActivity(i);
+                if(et_Usuario.getText().toString().isEmpty() || et_Contrasena.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),getResources().getString(R.string.log_wrong),Toast.LENGTH_SHORT).show();
+                }else{
+                    uname=et_Usuario.getText().toString();
+                    upass=et_Contrasena.getText().toString();
+                    mDB= FirebaseDatabase.getInstance().getReference().child(TAG_medicos).child(uname);
+                    mDB.child(TAG_contrasena).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot snapshot) {
+                            String psw= (String) snapshot.getValue();  //prints "Do you have data? You'll love Firebase."
+                            if(psw==null){//Si no se encontre una contraseña asociada al documetno ingresado
+                                Toast.makeText(logueo.this,getResources().getString(R.string.log_uwrong),Toast.LENGTH_SHORT).show();
+                            }else{
+                                if(psw.equals(upass)){//Si las contraseñas coinciden
+                                    preferenceEditor.putBoolean("flag",true);
+                                    preferenceEditor.commit();
+                                    preferenceEditor.putString("user",uname);
+                                    preferenceEditor.commit();
+                                    et_Usuario.setText("");
+                                    et_Contrasena.setText("");
+                                    Intent i =new Intent(logueo.this, MainActivity.class);
+                                    i.putExtra("usuario",uname);
+                                    startActivity(i);
+                                    finish();
+                                }else{
+                                    Toast.makeText(logueo.this,getResources().getString(R.string.log_pwrong),Toast.LENGTH_SHORT).show();
+                                    et_Contrasena.setText("");
+                                }
+                            }
+                        }
+                        @Override public void onCancelled(DatabaseError error) {
+                            Log.w(TAG, "loadPost:onCancelled", error.toException());
+                            Toast.makeText(logueo.this, "Failed to load data.",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+
+
             }
         });
 
